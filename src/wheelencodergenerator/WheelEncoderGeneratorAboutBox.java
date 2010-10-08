68834b47bfce162f934ce6a33aaaae37f2c9c230
shimniok <shimniok@3d4108e3-a8fe-e0eb-32f2-109a90f27b84>
2010-10-08 16:10:46 +0000

    Minor UI fixes, modified about box with proper info.

--- src/wheelencodergenerator/WheelEncoderGeneratorAboutBox.java
+++ src/wheelencodergenerator/WheelEncoderGeneratorAboutBox.java
@@ -98,7 +98,7 @@ public class WheelEncoderGeneratorAboutBox extends javax.swing.JDialog {
                             .addComponent(appVendorLabel)
                             .addComponent(appHomepageLabel)))
                     .addComponent(appTitleLabel, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(appDescLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
+                    .addComponent(appDescLabel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE)
                     .addComponent(closeButton))
                 .addContainerGap())
         );
