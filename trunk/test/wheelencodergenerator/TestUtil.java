96fdf5f4e230231cc73d9076c3fa123d2dfb57dd
shimniok@gmail.com <shimniok@gmail.com@3d4108e3-a8fe-e0eb-32f2-109a90f27b84>
2012-11-01 04:55:13 +0000

    Updated tests to reflect recent changes.

--- trunk/test/wheelencodergenerator/TestUtil.java
+++ trunk/test/wheelencodergenerator/TestUtil.java
@@ -5,8 +5,11 @@
 
 package wheelencodergenerator;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.junit.Assert;
 
 /**
  *
@@ -25,5 +28,25 @@ public class TestUtil {
             Logger.getLogger(ImageExportChooserTest.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
+    public static void createFile(File f) {
+        if (!f.exists()) {
+            System.out.println("saveAndQuit() creating "+f.getName());
+            try {
+                if (!f.createNewFile()) {
+                    Assert.fail();
+                }
+            } catch (IOException ex) {
+                Assert.fail();
+            }
+        }
+    }
+
+    public static void deleteFile(File f) {
+        if (f.exists()) {
+            System.out.println("saveAndQuit() Deleting "+f.getName());
+            f.delete();
+        }
+    }
+
 }
